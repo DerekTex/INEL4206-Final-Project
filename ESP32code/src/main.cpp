4c59837307cedecbffb453098babcf576d7203d4
Gabriel A. Lozada Torres <98755609+GLOZADA00@users.noreply.github.com>
2023-05-03 01:30:05 -0400

    Cambios en el codigo

--- ESP32code/src/main.cpp
+++ ESP32code/src/main.cpp
@@ -3,31 +3,89 @@
 #include <WiFi.h>
 #include <driver/gpio.h>
 #include <freeRTOS/FreeRTOS.h>
+#include <Ultrasonic.h>
 
-#define GPIO_NUMBER_36
-#define GPIO_NUMBER_32
 #define LED_BUILTIN 2
 #define WIFI_NAME "Gabriel's Galaxy S23"
 #define WIFI_PASSWORD "ddfe1234"
 #define WIFI_TIMEOUT 10000
+#define trigPin 13 
+#define echoPin 14 
+#define MAX_DISTANCE 700 
+float timeOut = MAX_DISTANCE * 60; 
+int soundVelocity = 340;
+
 
 const char *mqttServer = "esp32proj.space";
 const char *TempC = "TempC";
 const char *TempF = "TempF";
+const char *People = "People";
 const int mqttPort = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 int calibration = 0;
+int counter = 0;
+int people = 0;
 bool recalibrated = false;
 float recalibratedVoltage = 0;
 float temperatureQuotient = 0;
 float recalibratingVals[5];
+float prevDistance = 0;
 float highestTemp(float values[5]);
 float lowestTemp(float values[5]);
 float recalibrate(float values[5]);
 void connectToWifi();
+
+float recalibrate(float values[])
+{
+  float lowestTemperature = lowestTemp(values);
+  float highestTemperature = highestTemp(values);
+  float averageTemperature = (lowestTemperature + highestTemperature) / 2;
+
+  float interTemp = lowestTemperature + ((5 / 2) - 1) * (highestTemperature - lowestTemperature) / (5 - 1);//Interpolation to find the temperature of the thermistor
+  float temperatureQuotient = interTemp / averageTemperature;
+
+  return temperatureQuotient;
+}
+
+float lowestTemp(float values[])
+{
+  float lowestTemperature = values[0];
+  for (unsigned int i = 0; i < 5; i++)
+  {
+    if (values[i] < lowestTemperature)
+    {
+      lowestTemperature = values[i];
+    }
+  }
+  return lowestTemperature;
+}
+
+float highestTemp(float values[])
+{
+  float highestTemperature = values[0];
+  for (unsigned int i = 0; i < 5; i++)
+  {
+    if (values[i] > highestTemperature)
+    {
+      highestTemperature = values[i];
+    }
+  }
+  return highestTemperature;
+}
+
+float getSonar() {
+  unsigned long pingTime;
+  float distance;
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  pingTime = pulseIn(echoPin, HIGH, timeOut); 
+  distance = (float)pingTime * soundVelocity / 2 / 10000; 
+  return distance;
+}
 
 void connectToWifi()
 {
@@ -76,12 +134,15 @@ void connectToBroker()
   }
 }
 
+
 void setup()
 {
   Serial.begin(115200);
   connectToWifi();
   connectToBroker();
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT); 
 }
 
 void loop()
@@ -118,7 +179,15 @@ void loop()
     }
     delay(500);
   }
-  Serial.printf("Calibration: %d  \tTemperature : %.2fC    %.2fF\n", calibration, tempC, tempF);
+  
+  float distance = getSonar();
+  if (distance < prevDistance)
+  {
+    people++;
+  }
+  prevDistance = distance;
+
+  Serial.printf("People in the room: %d \tCalibration: %d  \tTemperature : %.2fC    %.2fF\n", people, calibration, tempC, tempF);
   delay(1000);
 
   if (!client.connected())
@@ -127,44 +196,8 @@ void loop()
   }
   client.publish(TempC, String(tempC).c_str());
   client.publish(TempF, String(tempF).c_str());
-  client.publish(getSonar,String())
+  client.publish(People,String(people).c_str());
   client.loop();
-}
 
-float recalibrate(float values[])
-{
-  float lowestTemperature = lowestTemp(values);
-  float highestTemperature = highestTemp(values);
-  float averageTemperature = (lowestTemperature + highestTemperature) / 2;
-
-  float interTemp = lowestTemperature + ((5 / 2) - 1) * (highestTemperature - lowestTemperature) / (5 - 1);//Interpolation to find the temperature of the thermistor
-  float temperatureQuotient = interTemp / averageTemperature;
-
-  return temperatureQuotient;
-}
-
-float lowestTemp(float values[])
-{
-  float lowestTemperature = values[0];
-  for (unsigned int i = 0; i < 5; i++)
-  {
-    if (values[i] < lowestTemperature)
-    {
-      lowestTemperature = values[i];
-    }
-  }
-  return lowestTemperature;
-}
-
-float highestTemp(float values[])
-{
-  float highestTemperature = values[0];
-  for (unsigned int i = 0; i < 5; i++)
-  {
-    if (values[i] > highestTemperature)
-    {
-      highestTemperature = values[i];
-    }
-  }
-  return highestTemperature;
+  delay(100);
 }
